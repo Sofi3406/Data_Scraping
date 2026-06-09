9ba9876a87d4581f2d0cf62af6d441fa462fc95b
Sofi3406 <sofiyasin190@gmail.com>
2026-06-09 15:47:47 -0700

    feat: Awash pro

---
+++ Awash Birr Pro.py
@@ -0,0 +1,21 @@
+from google_play_scraper import reviews_all, Sort
+import pandas as pd
+
+app_id = "com.sc.awashpay"
+
+result = reviews_all(
+    app_id,
+    lang="en",
+    country="et",
+    sort=Sort.NEWEST
+)
+
+print("Total reviews:", len(result))
+
+df = pd.DataFrame(result)
+
+df = df[["userName", "reviewId", "content", "score", "at"]]
+df.columns = ["name", "review_id", "review", "rating", "date"]
+
+df.to_csv("awash_birr_pro_reviews.csv", index=False, encoding="utf-8-sig")
+print(df.head())
